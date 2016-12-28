e5d63b05ab505cfc3eda1aece4e310e5780e3d1e
liubingxin <liubingxin1030@outlook.com>
2016-12-28 08:30:58 +0800

    Initial Commit

---
+++ CopyDeviceInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CopyDeviceInfo
+//
+//  Created by 刘丙鑫 on 2016/12/28.
+//  Copyright © 2016年 naver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CopyDeviceInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CopyDeviceInfo
+//
+//  Created by 刘丙鑫 on 2016/12/28.
+//  Copyright © 2016年 naver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
